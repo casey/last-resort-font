822132e56f9a5109517ac3429f054d37425b8c9f
Casey Rodarmor <casey@rodarmor.com>
2025-12-25 16:44:46 -0800

    Build LastResortPrivate mapping private use area to Last Resort Glyphs

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 # Remove old versions
-for file in LastResort*-Regular.otf LastResort*-Regular.ttf
+for file in LastResortPrivate*-Regular.otf LastResortPrivate*-Regular.ttf
 do
     if [[ -f $file ]]
     then
@@ -13,24 +13,24 @@ do
     fi
 done
 
-# Build FORMAT 13 (Last Resort High-Efficiency; LastResortHE-Regular.ttf)
+# Build FORMAT 13 (Last Resort Private High-Efficiency; LastResortPrivateHE-Regular.ttf)
 
 # Build a temporary name-keyed OpenType/CFF font (OTF)
-makeotf -r -nS -nshw -overrideMenuNames -f font.ufo -ff features.fea -omitMacNames -omitDSIG -gf GlyphOrderAndAliasDB -o LastResortHE-Regular.otf
+makeotf -r -nS -nshw -overrideMenuNames -f font.ufo -ff features.fea -omitMacNames -omitDSIG -gf GlyphOrderAndAliasDB -o LastResortPrivateHE-Regular.otf
 
 # Convert the OTF to TTF
-otf2ttf -o temp.ttf LastResortHE-Regular.otf
+otf2ttf -o temp.ttf LastResortPrivateHE-Regular.otf
 
 # Replace the 'cmap' table with one that includes a Format 13 subtable
 ttx -m temp.ttf -o temp2.ttf cmap-f13.ttx
 
 # Replace the 'OS/2' and 'head' tables
-ttx -m temp2.ttf -o LastResortHE-Regular.ttf tables.ttx
+ttx -m temp2.ttf -o LastResortPrivateHE-Regular.ttf tables.ttx
 
 # Clean up
-rm LastResortHE-Regular.otf temp.ttf temp2.ttf
+rm LastResortPrivateHE-Regular.otf temp.ttf temp2.ttf
 
-# Build FORMAT 12 (Last Resort; LastResort-Regular.ttf)
+# Build FORMAT 12 (Last Resort Private; LastResortPrivate-Regular.ttf)
 
 # The "lastresort13to12.py" script converts the Format 13 'cmap' subtable
 # to a Format 12 subtable, and generates an AFDKO "mergefonts" mapping file
@@ -40,23 +40,23 @@ rm LastResortHE-Regular.otf temp.ttf temp2.ttf
 
 python3 scripts/lastresort13to12.py cmap-f13.ttx
 mergefonts font2.ufo mergefonts.map font.ufo
-sed -i "" "s/ResortHE/Resort/g ; s/ High-Efficiency//g" font2.ufo/fontinfo.plist
+sed -i "" "s/LastResortPrivateHE-Regular/LastResortPrivate-Regular/g ; s/Last Resort Private High-Efficiency/Last Resort Private/g" font2.ufo/fontinfo.plist
 sed "s/ High-Efficiency//g" < features.fea > features2.fea
 
 # Build a temporary name-keyed OpenType/CFF font (OTF)
-makeotf -r -nS -nshw -overrideMenuNames -f font2.ufo -ff features2.fea -omitMacNames -omitDSIG -gf GlyphOrderAndAliasDB2 -o LastResort-Regular.otf
+makeotf -r -nS -nshw -overrideMenuNames -f font2.ufo -ff features2.fea -omitMacNames -omitDSIG -gf GlyphOrderAndAliasDB2 -o LastResortPrivate-Regular.otf
 
 # Convert the OTF to TTF
-otf2ttf -o temp.ttf LastResort-Regular.otf
+otf2ttf -o temp.ttf LastResortPrivate-Regular.otf
 
 # Replace the 'cmap' table with one that includes a Format 12 subtable
 ttx -m temp.ttf -o temp2.ttf cmap-f12.ttx
 
 # Replace the 'OS/2' and 'head' tables
-ttx -m temp2.ttf -o LastResort-Regular.ttf tables.ttx
+ttx -m temp2.ttf -o LastResortPrivate-Regular.ttf tables.ttx
 
 # Clean up
-rm LastResort-Regular.otf temp.ttf temp2.ttf GlyphOrderAndAliasDB2 cmap-f12.ttx features2.fea mergefonts.map
+rm LastResortPrivate-Regular.otf temp.ttf temp2.ttf GlyphOrderAndAliasDB2 cmap-f12.ttx features2.fea mergefonts.map
 rm -rf font2.ufo
 
 # EOF
